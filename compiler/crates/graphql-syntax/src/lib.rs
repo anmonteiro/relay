a005a70d2542b043dcadd60895fcac0680e9ee7b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-01-12 21:27:02 -0800

    remove unused import

--- compiler/crates/graphql-syntax/src/lib.rs
+++ compiler/crates/graphql-syntax/src/lib.rs
@@ -26,7 +26,7 @@ pub use parser::FragmentArgumentSyntaxKind;
 pub use parser::ParserFeatures;
 pub use source::GraphQLSource;
 pub use syntax_error::SyntaxError;
-pub use utils::*;
+// pub use utils::*;
 
 use crate::parser::Parser;
 
